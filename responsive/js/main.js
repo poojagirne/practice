42973fcd13e539ac08c2af9ff2799a2c69681310
Pooja <79736821+poojagirne@users.noreply.github.com>
2021-06-18 05:53:50 -0700

    Add files via upload

---
+++ responsive/js/main.js
@@ -0,0 +1,9 @@
+$('.slide-one')
+.not('.slick-intialized')
+.slick({
+    autoplay:true,
+    autoplaySpeed:1000,
+    dots:true,
+    prevArrow:'.site-slider .slider-btn .prev',
+    nextArrow:'.site-slider .slider-btn .next',
+});
